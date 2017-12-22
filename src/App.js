3c96e8412ac3eccc219db683879e63a48ab4657b
Brian Bycynski <high5bri@hotmail.com>
2017-12-22 12:46:20 -0500

    started grid project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="container">
+                <div className="sidebar">Sidebar</div>
+                <header className="header">Header</header>
+                <div className="realtors">Top 3 Realtors</div>
+                <section className="features">Features</section>
+                <div className="story__pictures">Story Pictures</div>
+                <div className="story__content">Story Content</div>
+                <section className="homes">Homes</section>
+                <section className="gallery">Gallery</section>
+                <footer className="footer">Footer</footer>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
