902992e11c5d20daba32bd10b6c2629f5d2776c9
Brian Bycynski <high5bri@hotmail.com>
2017-12-26 10:15:05 -0500

    finished lecture 116

--- src/App.js
+++ src/App.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react'
 
 import story1 from './img/story-1.jpeg'
 import story2 from './img/story-2.jpeg'
+import home1 from './img/house-1.jpeg'
+import home2 from './img/house-2.jpeg'
+import home3 from './img/house-3.jpeg'
+import home4 from './img/house-4.jpeg'
+import home5 from './img/house-5.jpeg'
+import home6 from './img/house-6.jpeg'
+
+import gal1 from './img/gal-1.jpeg'
+import gal2 from './img/gal-2.jpeg'
+import gal3 from './img/gal-3.jpeg'
+import gal4 from './img/gal-4.jpeg'
+import gal5 from './img/gal-5.jpeg'
+import gal6 from './img/gal-6.jpeg'
+import gal7 from './img/gal-7.jpeg'
+import gal8 from './img/gal-8.jpeg'
+import gal9 from './img/gal-9.jpeg'
+import gal10 from './img/gal-10.jpeg'
+import gal11 from './img/gal-11.jpeg'
+import gal12 from './img/gal-12.jpeg'
+import gal13 from './img/gal-13.jpeg'
+import gal14 from './img/gal-14.jpeg'
 
 class App extends Component {
     render() {
@@ -97,8 +118,155 @@ class App extends Component {
                     <button className="btn">Find Your Own Home</button>
                 </div>
 
-                <section className="homes">Homes</section>
-                <section className="gallery">Gallery</section>
+                <section className="homes">
+                    <div className="home">
+                        <img src={home1} alt="" className="home__img" />
+                        <h5 className="home__name">Beautiful family Home</h5>
+                        <div className="home__location" />
+                        <p>USA</p>
+                        <div className="home__rooms" />
+                        <p>5 Rooms</p>
+                        <div className="home__area" />
+                        <p>
+                            325 m<sup>2</sup>
+                        </p>
+                        <div className="home__price" />
+                        <p>$1,200,000</p>
+                        <button className="btn home__btn">
+                            Contact Realtors
+                        </button>
+                    </div>
+                    <div className="home">
+                        <img src={home2} alt="" className="home__img" />
+                        <h5 className="home__name">Beautiful family Home</h5>
+                        <div className="home__location" />
+                        <p>USA</p>
+                        <div className="home__rooms" />
+                        <p>5 Rooms</p>
+                        <div className="home__area" />
+                        <p>
+                            325 m<sup>2</sup>
+                        </p>
+                        <div className="home__price" />
+                        <p>$1,200,000</p>
+                        <button className="btn home__btn">
+                            Contact Realtors
+                        </button>
+                    </div>
+                    <div className="home">
+                        <img src={home3} alt="" className="home__img" />
+                        <h5 className="home__name">Beautiful family Home</h5>
+                        <div className="home__location" />
+                        <p>USA</p>
+                        <div className="home__rooms" />
+                        <p>5 Rooms</p>
+                        <div className="home__area" />
+                        <p>
+                            325 m<sup>2</sup>
+                        </p>
+                        <div className="home__price" />
+                        <p>$1,200,000</p>
+                        <button className="btn home__btn">
+                            Contact Realtors
+                        </button>
+                    </div>
+                    <div className="home">
+                        <img src={home4} alt="" className="home__img" />
+                        <h5 className="home__name">Beautiful family Home</h5>
+                        <div className="home__location" />
+                        <p>USA</p>
+                        <div className="home__rooms" />
+                        <p>5 Rooms</p>
+                        <div className="home__area" />
+                        <p>
+                            325 m<sup>2</sup>
+                        </p>
+                        <div className="home__price" />
+                        <p>$1,200,000</p>
+                        <button className="btn home__btn">
+                            Contact Realtors
+                        </button>
+                    </div>
+                    <div className="home">
+                        <img src={home5} alt="" className="home__img" />
+                        <h5 className="home__name">Beautiful family Home</h5>
+                        <div className="home__location" />
+                        <p>USA</p>
+                        <div className="home__rooms" />
+                        <p>5 Rooms</p>
+                        <div className="home__area" />
+                        <p>
+                            325 m<sup>2</sup>
+                        </p>
+                        <div className="home__price" />
+                        <p>$1,200,000</p>
+                        <button className="btn home__btn">
+                            Contact Realtors
+                        </button>
+                    </div>
+                    <div className="home">
+                        <img src={home6} alt="" className="home__img" />
+                        <h5 className="home__name">Beautiful family Home</h5>
+                        <div className="home__location" />
+                        <p>USA</p>
+                        <div className="home__rooms" />
+                        <p>5 Rooms</p>
+                        <div className="home__area" />
+                        <p>
+                            325 m<sup>2</sup>
+                        </p>
+                        <div className="home__price" />
+                        <p>$1,200,000</p>
+                        <button className="btn home__btn">
+                            Contact Realtors
+                        </button>
+                    </div>
+                </section>
+
+                <section className="gallery">
+                    <figure className="gallery__item gallery__item--1">
+                        <img src={gal1} alt="" className="gallery__img" />
+                    </figure>
+                    <figure className="gallery__item gallery__item--2">
+                        <img src={gal2} alt="" className="gallery__img" />
+                    </figure>
+                    <figure className="gallery__item gallery__item--3">
+                        <img src={gal3} alt="" className="gallery__img" />
+                    </figure>
+                    <figure className="gallery__item gallery__item--4">
+                        <img src={gal4} alt="" className="gallery__img" />
+                    </figure>
+                    <figure className="gallery__item gallery__item--5">
+                        <img src={gal5} alt="" className="gallery__img" />
+                    </figure>
+                    <figure className="gallery__item gallery__item--6">
+                        <img src={gal6} alt="" className="gallery__img" />
+                    </figure>
+                    <figure className="gallery__item gallery__item--7">
+                        <img src={gal7} alt="" className="gallery__img" />
+                    </figure>
+                    <figure className="gallery__item gallery__item--8">
+                        <img src={gal8} alt="" className="gallery__img" />
+                    </figure>
+                    <figure className="gallery__item gallery__item--9">
+                        <img src={gal9} alt="" className="gallery__img" />
+                    </figure>
+                    <figure className="gallery__item gallery__item--10">
+                        <img src={gal10} alt="" className="gallery__img" />
+                    </figure>
+                    <figure className="gallery__item gallery__item--11">
+                        <img src={gal11} alt="" className="gallery__img" />
+                    </figure>
+                    <figure className="gallery__item gallery__item--12">
+                        <img src={gal12} alt="" className="gallery__img" />
+                    </figure>
+                    <figure className="gallery__item gallery__item--13">
+                        <img src={gal13} alt="" className="gallery__img" />
+                    </figure>
+                    <figure className="gallery__item gallery__item--14">
+                        <img src={gal14} alt="" className="gallery__img" />
+                    </figure>
+                </section>
                 <footer className="footer">Footer</footer>
             </div>
         )
